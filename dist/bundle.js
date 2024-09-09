68b2b42b25cd786713bb0c6adfd86b275af82bcb
apustovoitova <anastasiapustovoitova@gmail.com>
2024-09-09 08:57:36 +0200

    added scrollToTopButton and JavaScript functionality

---
+++ dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.getElementById("button-top").addEventListener("click",(()=>{window.scrollTo({top:0,behavior:"smooth"})}))})();
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "normalize.css";
 import "./styles/main.scss";
+
+const scrollToTopButton = document.getElementById("button-top");
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
